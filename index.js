318dba72f098e64e32a272f56d5ab02274661719
jeylanab <jeylantesi@gmail.com>
2024-06-04 16:24:28 +0530

    added file

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
+import { error } from "console";
 
+inquirer
+    .prompt([
+       { message: "type the URL"},
+       { name : "URL"},
+
+    ]).then((answers) => {
+        const url = answers.URL;
+
+        let urqr = qr.image(url)
+        urqr.pipe(fs.createWriteStream("myimage.png"))
+
+    }).catch((error) => {
+        if (error) {
+            
+        }
+        else {
+            
+        }
+    })
